5fe21c8a937e2a76c4cc53c9e8ae993e473efeeb
Tanner Palmer <tannerp@ifit.com>
2022-04-01 17:48:44 -0600

    add useNavigate hook to go back one page on cancel click

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,7 +1,79 @@
+import "./signUp.css";
+import { useNavigate } from "react-router-dom";
+
 export default function SignUp() {
+  const navigate = useNavigate();
+
   return (
-    <main style={{ padding: "1rem 0", color: "white" }}>
-      <h2>Sign Up</h2>
-    </main>
+    <form style={{ backgroundColor: "white" }}>
+      <div class="conatiner">
+        <h1>Sign Up</h1>
+        <p> Please fill in this form to create an account</p>
+        <hr />
+        <label for="email">
+          <b>Email</b>
+        </label>
+        <input type="text" placeholder="Enter Email" name="email" required />
+
+        <label for="first-name">
+          <b>First Name</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter First Name"
+          name="first-name"
+          required
+        />
+
+        <label for="last-name">
+          <b>Last Name</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Last Name"
+          name="last-name"
+          required
+        />
+
+        <label for="username">
+          <b>Username</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          required
+        />
+
+        <label for="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
+
+        <label for="psw-repeat">
+          <b>Repeat Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="psw-repeat"
+          required
+        />
+
+        <div class="clearfix">
+          <button type="button" class="cancelbtn" onClick={() => navigate(-1)}>
+            Cancel
+          </button>
+          <button type="submit" class="signupbtn">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
